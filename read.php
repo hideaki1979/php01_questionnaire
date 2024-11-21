cc484b276e8c8206aa8a9f751a074b7c3f86ac20
hideaki1979 <syumeikyo2000@yahoo.co.jp>
2024-11-21 11:05:31 +0900

    README追加とさくらでchart.jsが動かない事象対応中

--- read.php
+++ read.php
@@ -75,6 +75,8 @@
 
 <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
 <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script>
+<!-- <script src="js/chart.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels@2.0.0"></script> -->
 <script>
     const ramenJson = JSON.parse('<?=$ramenJson ?>');
     console.log(ramenJson);
